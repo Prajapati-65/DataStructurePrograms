b5679622e253fb457d3b578c583eaeca90deba1a
om65praj <om65praj@gmail.com>
2017-09-11 19:36:32 +0530

    commit

--- src/com/bridgelabz/Programs/Calendar.java
+++ src/com/bridgelabz/Programs/Calendar.java
@@ -14,21 +14,25 @@ public class Calendar {
 		int month = utility.inputInteger();
 		System.out.print("Enter year : ");
 		int year = utility.inputInteger();
-		String[] months = { "", "January", "February", "March", "April", "May", "June", "July", "August", "September",
-				"October", "November", "December" };
-		int[] days = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+		String[] months = new String[] { "", "January", "February", "March", "April", "May", "June", "July", "August",
+				"September", "October", "November", "December" };
+
+		int[] days = new int[]{ 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 		if (month == 2 && utility.isLeapYear(year))
+		{
 			days[month] = 29;
-		System.out.println("  " + months[month] + " " + year);
+		}
+		System.out.println("   " + months[month] + " " + year);
 		System.out.println(" S  M Tu  W Th  F  S");
 		int d = utility.day(month, 1, year);
 		for (int i = 0; i < d; i++)
-			System.out.println(" ");
+			System.out.print("   ");
 		for (int i = 1; i <= days[month]; i++) {
-			System.out.print(" " + i);
-			if (((i + d) % 7 == 0) || (i == days[month])) {
-				System.out.println(" ");
-			}
+			System.out.printf("%2d ", i);
+			if (((i + d) % 7 == 0) || (i == days[month]))
+				System.out.println();
 		}
+
 	}
 }
